79f01be54ea8895bdf13db91641214b0e5df8132
ricardobap15 <josersb15@gmail.com>
2020-07-06 12:51:04 -0300

    commit

---
+++ pagina/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = document.querySelector('#msg')
+    var img = document.querySelector('#imagem')
+    var data = new Date()
+    var hora = 20
+     msg2.innerHTML = `Agora são <strong>${hora}:00</strong> horas. `
+    if (hora >= 4 && hora < 12) {
+        document.body.style.background = '#bed1b5'
+        img.src = 'fotomanha.jpg'
+        msg.innerHTML = 'Bom Dia!'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'fototarde.jpg'
+        document.body.style.background = '#c78f76'
+        msg.innerHTML = 'Boa Tarde!'
+    } else {
+        img.src = 'fotonoite.jpg'
+        document.body.style.background = '#2b2b23'
+        msg.innerHTML = 'Boa Noite!'
+    }
+}
